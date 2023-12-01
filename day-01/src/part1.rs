f68fd5f6863768d9bfb94180e74399393cafdebe
Sergei Kharchikov <callmestech@gmail.com>
2023-12-01 15:37:01 +0000

    day 01 part 1 solved

--- day-01/src/part1.rs
+++ day-01/src/part1.rs
@@ -1,8 +1,23 @@
 use crate::custom_error::AocError;
 
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String, AocError> {
-    todo!("day 01 - part 1");
+pub fn process(input: &str) -> miette::Result<String, AocError> {
+    let lines: Vec<_> = input.lines().collect();
+    let sum = lines
+        .iter()
+        .filter(|line| line.chars().any(char::is_alphanumeric))
+        .fold(0, |acc, &line| {
+            let mut digits = Vec::new();
+            line.chars().for_each(|c| {
+                if let Some(n) = c.to_digit(10) {
+                    digits.push(n)
+                }
+            });
+            let first = digits.first().unwrap_or(&0);
+            let last = digits.last().unwrap_or(first);
+            acc + (*first * 10 + *last)
+        });
+    Ok(sum.to_string())
 }
 
 #[cfg(test)]
@@ -11,9 +26,13 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "
+            1abc2
+            pqr3stu8vwx
+            a1b2c3d4e5f
+            treb7uchet
+        ";
+        assert_eq!("142", process(input)?);
         Ok(())
     }
 }
