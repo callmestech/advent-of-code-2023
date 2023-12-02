5b07ab336768ce82d8abe1bd7fa06f32b8316402
Sergei Kharchikov <callmestech@gmail.com>
2023-12-02 13:53:07 +0000

    day 01 part 2 solved

--- day-01/src/bin/part2.rs
+++ day-01/src/bin/part2.rs
@@ -1,12 +1,20 @@
-fn main() {
-    println!("Hello, world!");
-}
+use day_01::part2::process;
+use miette::Context;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+#[cfg(feature = "dhat-heap")]
+#[global_allocator]
+static ALLOC: dhat::Alloc = dhat::Alloc;
+
+#[tracing::instrument]
+fn main() -> miette::Result<()> {
+    #[cfg(feature = "dhat-heap")]
+    let _profiler = dhat::Profiler::new_heap();
+
+    #[cfg(not(feature = "dhat-heap"))]
+    tracing_subscriber::fmt::init();
+
+    let file = include_str!("../../input2.txt");
+    let result = process(file).context("process part 2")?;
+    println!("{}", result);
+    Ok(())
 }
--- day-01/src/part2.rs
+++ day-01/src/part2.rs
@@ -1,8 +1,44 @@
+use std::collections::HashMap;
+
 use crate::custom_error::AocError;
+use regex::Regex;
 
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String, AocError> {
-    todo!("day 01 - part 2");
+pub fn process(input: &str) -> miette::Result<String, AocError> {
+    let digits_map: HashMap<&str, u32> = HashMap::from([
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ]);
+    let regex = Regex::new(r"\d|one|two|three|four|five|six|seven|eight|nine").unwrap();
+    let lines: Vec<_> = input.lines().collect();
+    let sum = lines
+        .iter()
+        .filter(|line| line.chars().any(char::is_alphanumeric))
+        .fold(0, |acc, &line| {
+            let mut remaining = line;
+            let mut digits = Vec::new();
+            while let Some(m) = regex.find(remaining) {
+                let found_str = m.as_str();
+                let found = digits_map
+                    .get(found_str)
+                    .copied()
+                    .or(found_str.parse::<u32>().ok())
+                    .unwrap_or_default();
+                digits.push(found);
+                remaining = &remaining[m.start() + 1..]
+            }
+            let first = digits.first().unwrap_or(&0);
+            let last = digits.last().unwrap_or(first);
+            acc + (*first * 10 + *last)
+        });
+    Ok(sum.to_string())
 }
 
 #[cfg(test)]
@@ -11,9 +47,16 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "
+            two1nine
+            eightwothree
+            abcone2threexyz
+            xtwone3four
+            4nineeightseven2
+            zoneight234
+            7pqrstsixteen
+        ";
+        assert_eq!("281", process(input)?);
         Ok(())
     }
 }
